d93ac36e107dcb601c92bd64a9e1e411e80af58b
matheenece <142809192+matheenece@users.noreply.github.com>
2023-08-22 20:16:38 +0530

    Add files via upload
    commit

---
+++ homeAssignment/Fibbinocci.java
@@ -0,0 +1,25 @@
+package homeAssignment;
+
+public class Fibbinocci 
+{
+	public static void main(String[] args) 
+	{
+		int a = 0;
+		int b = 1;
+		int sum = 0;
+		
+		System.out.println("Fibbinocci series is: ");
+		System.out.println(a);
+		System.out.println(b);
+		
+		for (int i = 1; i < 10; i++)
+		{
+			sum = a+b;
+			a = b;
+			b = sum;
+			
+			System.out.println(sum);
+		}
+		
+	}
+}
